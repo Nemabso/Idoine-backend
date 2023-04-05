d0efb896a5b668231c06b39de2a4196d414c7cd6
LucasPitzalis <lpitzalis@yahoo.fr>
2023-04-05 13:20:52 +0200

    removed server side rendering

--- server.js
+++ server.js
@@ -19,15 +19,15 @@ connectDB().catch(err => console.log(err));
 app.use(cors());
 app.use(express.json());
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, process.env.CLIENT_PATH)));
+// // Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, process.env.CLIENT_PATH)));
 
 app.use('/api/review', reviewRouter);
 
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, process.env.CLIENT_PATH, 'index.html'));
-  });
+// // All other GET requests not handled before will return our React app
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, process.env.CLIENT_PATH, 'index.html'));
+// });
 
 app.listen(port, () => {
     console.log(`Server started at: http://localhost:${port}`);
