20ed4158d3e29a73a2bd00cc1bd3bf29736dc5ec
LucasPitzalis <lpitzalis@yahoo.fr>
2023-04-25 10:16:21 +0200

    stable version

--- server.js
+++ server.js
@@ -11,12 +11,21 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 
 connectDB().catch(err => console.log(err));
 
 app.use(express.json());
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, process.env.CLIENT_PATH)));
+
 app.use('/api/review', reviewRouter);
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, process.env.CLIENT_PATH, 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server started at: http://localhost:${port}`);
